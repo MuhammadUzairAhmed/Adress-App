08d6ef60d305d0825bb59767d65f03ed6a6be4b1
Muhammad Uzair Ahmed <uzair.ahmed@veturedive.com>
2021-05-12 16:56:26 +0500

    address app updated work

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      streetAddress
+      city
+      zipCode
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      streetAddress
+      city
+      zipCode
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      streetAddress
+      city
+      zipCode
+      createdAt
+      updatedAt
+    }
+  }
+`;
